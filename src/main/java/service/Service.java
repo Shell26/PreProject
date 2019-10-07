2937d4f9076431a91cfdc0a289ccbe42b31a4fbf
Shell26 <socialkii@yandex.ru>
2019-10-07 17:29:08 +0400

    Update

--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -3,94 +3,70 @@ package service;
 import dao.UserDAO;
 import exeption.DBExeption;
 import model.User;
+import util.BDConnection;
 
 import java.sql.*;
 import java.sql.SQLException;
 import java.util.List;
 
 public class Service {
-    public Service(){
+    public Service() {
 
     }
 
-    public void addUser(User user){
+    public void addUser(User user) {
         try {
-            getUserDAO().addUser(user);
+            new BDConnection().getUserDAO().addUser(user);
 //            user.setId(getUserDAO().userId(user));
-        }catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public List<User> getAllUsers(){
-        try{
-            return getUserDAO().getAllUsers();
-        }catch (SQLException e){
+    public List<User> getAllUsers() {
+        try {
+            return new BDConnection().getUserDAO().getAllUsers();
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
             return null;
         }
     }
 
-    public void deleteUser(User user){
-        try{
-            getUserDAO().deleteUser(user);
-        }catch (SQLException e){
-        System.out.println(e.getMessage());
-        }
-    }
-
-    public void updateUser (User user, String name, String secondName, Long age){
-        try{
-            getUserDAO().updateUser(user, name, secondName, age);
-        }catch (SQLException e){
+    public void deleteUser(User user) {
+        try {
+            new BDConnection().getUserDAO().deleteUser(user);
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public void createT(){
-        UserDAO dao = getUserDAO();
+    public void updateUser(User user, String name, String secondName, Long age) {
+        try {
+            new BDConnection().getUserDAO().updateUser(user, name, secondName, age);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void createT() {
+        UserDAO dao = new BDConnection().getUserDAO();
         try {
             dao.createTable();
-        }catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
     public void cleanUp() {
-        UserDAO dao = getUserDAO();
+        UserDAO dao = new BDConnection().getUserDAO();
         try {
             dao.dropTable();
-        }catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
-
-    private static Connection getMysqlConnection(){
-        try {
-            DriverManager.registerDriver((Driver) Class.forName("com.mysql.jdbc.Driver").newInstance());
-
-            StringBuilder url = new StringBuilder();
-
-            url.
-                    append("jdbc:mysql://").        //db type
-                    append("localhost:").           //host name
-                    append("3306/").                //port
-                    append("test1?").          //db name
-                    append("user=root&").          //login
-                    append("password=root");       //password
-
-            System.out.println("URL: " + url + "\n");
-
-            Connection connection;
-            connection = DriverManager.getConnection(url.toString());
-            return connection;
-        }catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-            e.printStackTrace();
-            throw new IllegalStateException();
-        }
-    }
-
-    private static UserDAO getUserDAO(){
-        return new UserDAO(getMysqlConnection());
-    }
 }
+
+
+
+
---
+++ src/main/java/util/BDConnection.java
@@ -0,0 +1,44 @@
+package util;
+
+import dao.UserDAO;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BDConnection {
+
+    public BDConnection(){
+
+    }
+
+    public static UserDAO getUserDAO(){
+        return new UserDAO(getMysqlConnection());
+    }
+
+    private static Connection getMysqlConnection(){
+        try {
+            DriverManager.registerDriver((Driver) Class.forName("com.mysql.jdbc.Driver").newInstance());
+
+            StringBuilder url = new StringBuilder();
+
+            url.
+                    append("jdbc:mysql://").        //db type
+                    append("localhost:").           //host name
+                    append("3306/").                //port
+                    append("test1?").          //db name
+                    append("user=root&").          //login
+                    append("password=root");       //password
+
+            System.out.println("URL: " + url + "\n");
+
+            Connection connection;
+            connection = DriverManager.getConnection(url.toString());
+            return connection;
+        }catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new IllegalStateException();
+        }
+    }
+}

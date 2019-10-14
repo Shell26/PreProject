5fe2d5a8e7b5f164fe1b0af8532d0f488ca23ea1
Shell26 <socialkii@yandex.ru>
2019-10-14 14:39:03 +0400

    Обновил в соответствии с 3-м заданием предпроекта

---
+++ src/main/java/dao/UserDaoFactory.java
@@ -0,0 +1,15 @@
+package dao;
+
+public class UserDaoFactory {
+
+    public static UserDAO getUserDao(String type){
+        if (type.equalsIgnoreCase("JDBC")){
+            return new UserDaoJDBCImpl();
+        }else if(type.equalsIgnoreCase("hibernate")){
+            return new UserDaoHibernateImpl();
+        }else {
+            return null;
+        }
+    }
+
+}
